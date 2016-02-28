ff7d7021470b7506f244d554255025a86eb14ee5
Rick2015 <Rick2015r@aol.com>
2016-02-27 22:07:38 -0600

    Added comments and data path
    Also removed an orphaned variable "source".

--- server.r
+++ server.r
@@ -2,7 +2,7 @@ library(shiny)
 library(xlsx)
 library(forecast)
 
-cpiU <- read.xlsx("../SeriesReport-20160224001724_bf095b.xlsx", sheetName = "BLS Data Series", startRow = 11, header = TRUE, endRow = 51)
+cpiU <- read.xlsx("./data/SeriesReport-20160224001724_bf095b.xlsx", sheetName = "BLS Data Series", startRow = 11, header = TRUE, endRow = 51)
 cpiU <- cpiU[-1,] #Remove 1977 row to align with 1978 
 cpiUts <- ts(cpiU$Annual, start = c(1978), end = c(2014), frequency = 1) #Time series obj of yearly average
 cpiUper <- cpiUts/lag(cpiUts,-1) - 1    #Annual inflation based on CPI-U
@@ -11,19 +11,17 @@ cpiUper <- cpiUts/lag(cpiUts,-1) - 1    #Annual inflation based on CPI-U
 shinyServer(
   function(input, output) {
     
-    output$sources <- renderPrint({"Data Sources"})
-    
     cpiUperm <- meanf(cpiUper, h=86, level = c(80,99), fan = FALSE, lambda = NULL) #86 yr Forecast from mean
     cpiUpern <- naive(cpiUper, h=86, level = c(80,99), fan = FALSE, lambda = NULL) #86 yr Forecast Naive method
     cpiUperd <- rwf(cpiUper, h=86, drift = TRUE, level = c(80,99), fan = FALSE, lambda = NULL)#86 yr Forecast from Random Walk with drift
     
     output$od2 <- renderPrint({input$id2})
     output$od3 <- renderPrint({ 
-      as.numeric(input$id3) - 2014 
+      as.numeric(input$id3) - 2014 #Calculate number of years
       })
     output$od4 <- renderPrint({
         p<-input$id1 #Principal
-        y<-as.numeric(input$id3) - 2014 #Years
+        y<-as.numeric(input$id3) - 2014 #Calculate number of years
         
       if (input$id2 == "Mean")
         for(i in 1:y) {  p <- as.numeric(p*cpiUperm$upper[i,2]) + p } #Mean Upper 99% compound interest
@@ -32,15 +30,16 @@ shinyServer(
       if (input$id2 == "Random Walk with Drift")
          for(i in 1:y) {  p <- as.numeric(p*cpiUperd$upper[i,2]) + p } #Random Upper 99% compound interest
          
-      paste0("$", round(p, digits=2))
+      paste0("$", round(p, digits=2)) #Return inflated result
       })
     
     output$newPlot <- renderPlot({
-        Y<-as.numeric(input$id3) - 2014 #Years
+        Y<-as.numeric(input$id3) - 2014 #Calculate number of years
+        #Recalculate forecast for specific year input
         cpiUm <- meanf(cpiUper, h=Y, level = c(80,99), fan = FALSE, lambda = NULL)
         cpiUn <- naive(cpiUper, h=Y, level = c(80,99), fan = FALSE, lambda = NULL)
         cpiUd <- rwf(cpiUper, h=Y, drift = TRUE, level = c(80,99), fan = FALSE, lambda = NULL)
-      
+        #Return custom plot
       if (input$id2 == "Mean")
          plot(cpiUm, xlab="Years", ylab="Delta", main = "Inflation Rate Forecast with Mean")
          legend("bottomleft",c("80 confidence level","99 confidence level","mean"),lwd=c(3,3,3), col=c("gray","gray60","blue"))
