6019b642d418e3b6a29a038af2342642dbd348a3
Rick2015 <Rick2015r@aol.com>
2016-02-27 21:29:46 -0600

    Added Shiny App to Repo
    This is the "Predict Inflation" app created for the Coursera Data
    Products project.

---
+++ ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  headerPanel("Predict Inflation"),
+  sidebarPanel(
+     h3('Parameters'),
+     numericInput('id1', '2014 U.S. Dollar Input $', 1, min = 1, max = 100000, step = 1),
+     selectInput('id2', 'Choose Forecast Method', c("Mean", "Naive", "Random Walk with Drift")),
+     selectInput('id3', 'Select Year', 2017:2100 ),
+     submitButton('Run Prediction'),
+     h3(''),
+     h5('Data Source:'), 
+     h6('Consumer Price Index (CPI-U) 1977-2014'),
+     h6('U.S.Bureau of Labor Statistics (BLS)'),
+     h6('http://www.bls.gov/cpi/')
+  ),
+  mainPanel(
+    h3('Results for Upper Inflation Only!'),
+    h4('Forecast Method'), verbatimTextOutput("od2"),
+    h4('Using compounding interest, input inflated to'), verbatimTextOutput("od4"),
+    h4('Years since 2014'), verbatimTextOutput("od3"),
+    plotOutput('newPlot')
+  )
+))
